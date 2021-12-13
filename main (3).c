b0cb513d81ffea7f7aadaca5bc8df54c34ce4db8
KAUSHAL CHAUHAN <93530886+kaushalch5287@users.noreply.github.com>
2021-12-13 22:15:58 +0530

    Add files via upload

---
+++ main (3).c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include<stdio.h>
+int main(){
+for(int i=1;i<=100;i++)
+ {
+ if(i%2==1)
+ {
+ printf("%d\n", i);
+ }
+ }
+ return 0;
+}
